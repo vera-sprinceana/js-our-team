aad4c409d1125836da2821a6a4c2816b6a141928
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-03-24 14:54:41 +0100

    Milestone-1

---
+++ asset/javascript/main.js
@@ -0,0 +1,73 @@
+
+let ourTeam=[
+    {
+        'nomi':'Wayne Barnett',
+        'ruoli':'Fonder & CEO',
+        'img':'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'nomi':'Angela Caroll',
+        'ruoli':'Chief Editor',
+        'img':'angela-caroll-chief-editor.jpg'
+    },
+    {
+        'nomi':'Walter Gordon',
+        'ruoli':'Office Manager',
+        'img':'walter-gordon-office-manager.jpg'
+    },
+    {
+        'nomi':'Angela Lopez',
+        'ruoli':'Social Media Manager',
+        'img':'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'nomi':'Scott Estrada',
+        'ruoli':'Developer',
+        'img':'scott-estrada-developer.jpg'
+    },
+    {
+        'nomi':'Barbara Ramos',
+        'ruoli':'Graphic Designer',
+        'img':'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+for(let i=0; i<ourTeam.length; i++){
+    
+    console.log(ourTeam[i])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // {
+    //     'nomi':'Barbara Ramos',
+    //     'ruoli':'Social Media Manager',
+    //     'img':'new-team-member-01.jpg'
+    // },
+    // {
+    //     'nomi':'Barbara Ramos',
+    //     'ruoli':'Graphic Designer',
+    //     'img':'new-team-member-02.jpg'
+    // },
+    // {
+    //     'nomi':'Barbara Ramos',
+    //     'ruoli':'Graphic Designer',
+    //     'img':'new-team-member-03.jpg'
+    // },
+    // {
+    //     'nomi':'Barbara Ramos',
+    //     'ruoli':'Graphic Designer',
+    //     'img':'new-team-member-04.jpg'
+    // }
