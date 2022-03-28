4b9a6a4e8b0216600fc5e92c1a588f4c68d6e7c9
vera.sprinceana <sprinceana.vera96@gmail.com>
2022-03-28 15:05:41 +0200

    es completo

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -3,42 +3,62 @@ let ourTeam=[
     {
         'nomi':'Wayne Barnett',
         'ruoli':'Fonder & CEO',
-        'img':'wayne-barnett-founder-ceo.jpg'
+        'immagine':'wayne-barnett-founder-ceo.jpg'
     },
     {
         'nomi':'Angela Caroll',
         'ruoli':'Chief Editor',
-        'img':'angela-caroll-chief-editor.jpg'
+        'immagine':'angela-caroll-chief-editor.jpg'
     },
     {
         'nomi':'Walter Gordon',
         'ruoli':'Office Manager',
-        'img':'walter-gordon-office-manager.jpg'
+        'immagine':'walter-gordon-office-manager.jpg'
     },
     {
         'nomi':'Angela Lopez',
         'ruoli':'Social Media Manager',
-        'img':'angela-lopez-social-media-manager.jpg'
+        'immagine':'angela-lopez-social-media-manager.jpg'
     },
     {
         'nomi':'Scott Estrada',
         'ruoli':'Developer',
-        'img':'scott-estrada-developer.jpg'
+        'immagine':'scott-estrada-developer.jpg'
     },
     {
         'nomi':'Barbara Ramos',
         'ruoli':'Graphic Designer',
-        'img':'barbara-ramos-graphic-designer.jpg'
+        'immagine':'barbara-ramos-graphic-designer.jpg'
     }
 ]
 //Milestone 2: stampare i dati all'interno di un contenitore
 // nella pagina html in modo dinamico,
 // creando per ciascun membro del team un elemento che conterrà i suoi dati.
- for(let i=0; i<ourTeam.length; i++){
-    console.log(ourTeam[i])
-      document.getElementById("containerDati").innerHTML+=`<div class="card"><h3>${ourTeam[i]['nomi']}</h3>   <p>${ourTeam[i]['ruoli']}</p> <p> ${ourTeam[i]['img']}</p></div>`;
- }
 
+for(let i=0; i<ourTeam.length; i++){
+    for(let key in ourTeam[i]){
+        console.log(`${key}->${ourTeam[i][key]} `)
+    }
+}
+
+ for(let i=0; i<ourTeam.length; i++){
+    
+    for(let key in ourTeam[i]){
+        console.log(ourTeam[i][key])
+        console.log[key] 
+    }   
+    document.getElementById("containerDati").innerHTML+=`
+        <div class="container containerCards>
+            <div class="cardS" style="width: 18rem;">
+                <img src=./asset/img//${ourTeam[i]['immagine']} class="card-img-top w_castom">
+                <div class="card-body text-center">
+                    <h5 class="card-title">${ourTeam[i]['nomi']}</h5>
+                    <p class="card-text">${ourTeam[i]['ruoli']}</p>
+                </div>
+            </div>
+        </div>
+      `      
+}
 
 
 
